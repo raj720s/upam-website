087596370d5f76b49470d3fb0b6768b6f2b853a8
tim-bresnon <bresnont@gmail.com>
2024-04-22 16:57:21 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import "../styles/index.css";
+import "../styles/prism-vsc-dark-plus.css";
+import ToasterContext from "./api/contex/ToasetContex";
+import { useEffect, useState } from "react";
+import PreLoader from "@/components/Common/PreLoader";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head >
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+      </head>
+
+      <body>
+        {loading ? (
+          <PreLoader />
+        ) : (
+          <SessionProvider>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <ToasterContext />
+                <Header />
+                <div className="mt-[100px]">
+              {children}
+                </div>
+              <Footer />
+              <ScrollToTop />
+            </ThemeProvider>
+          </SessionProvider>
+        )}
+      </body>
+    </html>
+  );
+}
